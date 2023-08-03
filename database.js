b762dd9a86ac3392140c594d6463b22b95100aff
kamsich02 <101313559+kamsich02@users.noreply.github.com>
2023-08-03 09:33:14 -0700

    second commit

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const postgres = require('postgres');
 require('dotenv').config();
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`;
 
 const pool = postgres(URL, { ssl: 'require' });
 
